2c2d252642f6e617266bcfd152c6a0e519e4cb26
Mis012 <Michael.Srba@seznam.cz>
2023-01-07 19:12:00 +0100

    bionic_translation/libc/libc-misc.c: die on 'setlocale' until we have a reason to believe it doesn't cause hard to diagnose issues

--- bionic_translation/libc/libc-misc.c
+++ bionic_translation/libc/libc-misc.c
@@ -1,8 +1,12 @@
+#include <errno.h>
+#include <locale.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 // this seems to not be a stable ABI, so hopefully nobody treats it as such
+// FIXME: move this out into it's own file once whenever we get to implementing it more properly
 struct prop_info {
 	const char *name;
 };
@@ -36,8 +40,46 @@ int bionic___system_property_read(const struct prop_info* prop_info, char* name,
 	return strlen(value);
 }
 
+// actually misc stuff
+
 void android_set_abort_message(const char* msg)
 {
 	printf("android_set_abort_message called: '%s'\n", msg);
 //	exit(1);
+}
+
+// setlocale is a bit special on bionic, try to mimic the behavior
+static bool __is_utf8_locale(const char* locale_name) {
+  return (*locale_name == '\0' || strstr(locale_name, "UTF-8"));
+}
+
+char *bionic_setlocale(int category, const char *locale) {
+	// TODO: unfuck our message printing, possibly by using android's logging library, so that we don't need to worry about messages getting missed
+	if(!getenv("UGLY_BIONIC_SETLOCALE_OVERRIDE")) {
+		fprintf(stderr, "bionic_setlocale: we don't know if this is behaving correctly, so we're crashing here (sorry);\n"
+			            "if you have found an app which uses this, please define the env `UGLY_BIONIC_SETLOCALE_OVERRIDE=`\n"
+			            "and make sure to suspect this function if something remotely locale related is broken\n"
+			            "most importantly, let us know on gitlab\n"
+			            "(the reason for crashing here is that simply printing a warning risks someone missing it and wasting time, \n"
+	                    "since broken locale can cause very hard to diagnose issues)\n");
+		exit(69);
+	}
+
+	// list of allowed locales from bionic
+	if(strcmp(locale, "") == 0 ||
+	   strcmp(locale, "C") == 0 ||
+	   strcmp(locale, "C.UTF-8") == 0 ||
+	   strcmp(locale, "en_US.UTF-8") == 0 ||
+	   strcmp(locale, "POSIX") == 0) {
+		if(__is_utf8_locale(locale)) {
+			setlocale(LC_ALL, "en_US.UTF-8");
+			return "C.UTF-8";
+		} else {
+			setlocale(LC_ALL, "en_US");
+			return "C";
+		}
+	} else {
+		errno = ENOENT;
+		return NULL;
+	}
 }
